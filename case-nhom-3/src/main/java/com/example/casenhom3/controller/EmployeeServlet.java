9943af9ccc4d924314edcc70bca7a24affb3e070
NamTrinh2207 <trinhnam180697@gmail.com>
2023-03-13 21:11:43 +0700

    update list employees

--- case-nhom-3/src/main/java/com/example/casenhom3/controller/EmployeeServlet.java
+++ case-nhom-3/src/main/java/com/example/casenhom3/controller/EmployeeServlet.java
@@ -1,19 +1,98 @@
 package com.example.casenhom3.controller;
 
+import com.example.casenhom3.model.Employee;
+import com.example.casenhom3.service.IEcommerce;
+import com.example.casenhom3.service.employee.EmployeeService;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "EmployeeServlet", value = "/employees")
 public class EmployeeServlet extends HttpServlet {
+    IEcommerce<Employee> iEcommerce;
+
+    @Override
+    public void init() throws ServletException {
+        iEcommerce = new EmployeeService();
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+        switch (action) {
+            case "create":
+                createForm(request, response);
+                break;
+            case "edit":
+                editForm(request, response);
+                break;
+            case "delete":
+                deleteForm(request, response);
+                break;
+            case "view":
+                view(request, response);
+                break;
+            default:
+                listEmployee(request, response);
+        }
+    }
 
+    private void listEmployee(HttpServletRequest request, HttpServletResponse response) {
+        List<Employee> employees = iEcommerce.findAll();
+        request.setAttribute("employees", employees);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("employee/list.jsp");
+        try {
+            dispatcher.forward(request, response);
+        } catch (ServletException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void view(HttpServletRequest request, HttpServletResponse response) {
+    }
+
+    private void deleteForm(HttpServletRequest request, HttpServletResponse response) {
+    }
+
+    private void editForm(HttpServletRequest request, HttpServletResponse response) {
+    }
+
+    private void createForm(HttpServletRequest request, HttpServletResponse response) {
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+        switch (action) {
+            case "create":
+                createEmployee(request, response);
+                break;
+            case "edit":
+                editEmployee(request, response);
+                break;
+            case "delete":
+                deleteEmployee(request, response);
+                break;
+            default:
+                listEmployee(request, response);
+        }
+    }
 
+    private void deleteEmployee(HttpServletRequest request, HttpServletResponse response) {
+    }
+
+    private void editEmployee(HttpServletRequest request, HttpServletResponse response) {
+    }
+
+    private void createEmployee(HttpServletRequest request, HttpServletResponse response) {
     }
 }
