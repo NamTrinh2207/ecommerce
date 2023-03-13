08f6fe5bc82cb5a4b83b19a6c4b979e412f3c859
Tuong <aumanhtuong2001@gmail.com>
2023-03-13 10:36:59 +0700

    them thuoc tinh cho Product $ cate

--- case-nhom-3/src/main/java/com/example/casenhom3/model/Product.java
+++ case-nhom-3/src/main/java/com/example/casenhom3/model/Product.java
@@ -1,4 +1,9 @@
 package com.example.casenhom3.model;
 
 public class Product {
+    private int id;
+    private String code;
+    private double price;
+    private String img;
+    private String describe;
 }
