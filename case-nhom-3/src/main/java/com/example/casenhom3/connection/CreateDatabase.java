24068ce8683751b1d94c8d64fd6d571bf5cd63b4
dinhnguyen96 <39048935+dinhnguyen96@users.noreply.github.com>
2023-03-14 22:02:30 +0700

    update create database

--- case-nhom-3/src/main/java/com/example/casenhom3/connection/CreateDatabase.java
+++ case-nhom-3/src/main/java/com/example/casenhom3/connection/CreateDatabase.java
@@ -33,8 +33,4 @@ public class CreateDatabase {
         return connection;
     }
 
-    public static void main(String[] args) {
-        Connection connection = CreateDatabase.getConnection();
-    }
-
 }
--- case-nhom-3/src/main/java/com/example/casenhom3/service/order/OrderServiceImpl.java
+++ case-nhom-3/src/main/java/com/example/casenhom3/service/order/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.casenhom3.service.order;
 
+import com.example.casenhom3.connection.CreateDatabase;
 import com.example.casenhom3.model.*;
 
 import java.sql.*;
