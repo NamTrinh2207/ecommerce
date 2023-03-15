9924e10435ed8e177d210f4c8e10c39eba412497
dinhnguyen96 <39048935+dinhnguyen96@users.noreply.github.com>
2023-03-15 22:30:31 +0700

    update Create Database

--- case-nhom-3/src/main/java/com/example/casenhom3/connection/CreateDatabase.java
+++ case-nhom-3/src/main/java/com/example/casenhom3/connection/CreateDatabase.java
@@ -6,16 +6,16 @@ import java.sql.SQLException;
 
 public class CreateDatabase {
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String URL = "jdbc:mysql://localhost:3306/ecommerce?useUnicode=yes&characterEncoding=UTF-8";
-    private static final String userNam = "Nam";
-    private static final String passNam = "Anhnam220797anhnam";
+    private static final String URL = "jdbc:mysql://localhost:3306/productmanager_nhom3?useUnicode=yes&characterEncoding=UTF-8";
+    private static final String userNguyen = "root";
+    private static final String passNguyen = "Nguyen251096*";
 
     public static Connection getConnection() {
         Connection connection = null;
         try {
             Class.forName(DRIVER);
-            connection = DriverManager.getConnection(URL, userNam, passNam);
-            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(URL, userNguyen, passNguyen);
+            System.out.println("connection successfully");
         } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
         }
