6b70ec9a1728c2e2dc865ef82a55c08cd8aab8b0
lê đoàn <huongly95967@gmail.com>
2023-03-16 10:36:52 +0700

    update

--- case-nhom-3/src/main/java/com/example/casenhom3/connection/CreateDatabase.java
+++ case-nhom-3/src/main/java/com/example/casenhom3/connection/CreateDatabase.java
@@ -8,12 +8,7 @@ public class CreateDatabase {
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/ecommerce?useUnicode=yes&characterEncoding=UTF-8";
     private static final String userNguyen = "root";
-<<<<<<< HEAD
-    private static final String passNguyen = "tuong2001";
-=======
     private static final String passNguyen = "10061995";
->>>>>>> 237e8a5b50f56fa138b9e7b824e17a1def2a1fd4
-
     public static Connection getConnection() {
         Connection connection = null;
         try {
